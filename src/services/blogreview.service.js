822916a0865a7016be124b24bd002d2bef10e89c
Chawapon <chawapon2001@gmail.com>
2022-03-24 20:41:31 +0700

    Update blogreview.service.js

--- src/services/blogreview.service.js
+++ src/services/blogreview.service.js
@@ -20,11 +20,10 @@ const validate = (body) => {
 
 	return schema.validate(body);
 };
-const validate_like_dislike = (body) => {
-	const schema = Joi.object({ _id: Joi.string(), userId: Joi.string() });
+// const validate_like_dislike = (body) => {
+// 	const schema = Joi.object({ _id: Joi.string(), userId: Joi.string() });
 
-	return schema.validate(body);
-};
+// 	return schema.validate(body);
+// };
 
 exports.validate = validate;
-exports.validate_like_dislike = validate_like_dislike;
