e78b2a7af6ce214e5677a2ce3618aeb83cdc6ef5
gandastik <gandastik@gmail.com>
2022-04-12 19:33:21 +0700

    change route /api/subject to query with params

--- src/routes/subject.route.js
+++ src/routes/subject.route.js
@@ -3,7 +3,7 @@ const subjectController = require('../controllers/subject.controller');
 const router = require('express').Router();
 
 router.get('/', async (req, res) => {
-	const { id } = req.body;
+	const { id } = req.query;
 	const subject = await subjectController.findSubjectById(id);
 
 	res.send(subject);
