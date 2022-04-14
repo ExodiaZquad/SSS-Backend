bcd22941c2d0268c03fe101bd31ecd06b857de8a
zantaclaus <tarzuto@gmail.com>
2022-04-14 22:51:39 +0700

    Implement Subject filter, fix status of findSubjectById.

--- src/routes/subject.route.js
+++ src/routes/subject.route.js
@@ -1,12 +1,38 @@
+const combinations = require('combinations');
 const subjectController = require('../controllers/subject.controller');
-
+const scheduleController = require('../controllers/schedule.controller');
+const { isSameIdInList } = require('../services/schedule.service');
 const router = require('express').Router();
 
 router.get('/', async (req, res) => {
 	const { id } = req.query;
 	const subject = await subjectController.findSubjectById(id);
+	if (!subject) return res.status(400).send({ success: false });
 
-	res.send(subject);
+	return res.send(subject);
+});
+
+router.post('/filter', async (req, res) => {
+	const { subjects } = req.body;
+
+	const theories = await subjectController.findTheoriesByIdAndSec(subjects);
+	if (!theories) return res.status(400).send({ success: 1 });
+
+	const labs = await subjectController.findLabs();
+	if (!labs) return res.status(400).send({ success: 2 });
+
+	let combination = combinations(theories).filter(
+		(temp) => temp.length == subjects.length && !isSameIdInList(temp),
+	);
+
+	const generated = scheduleController.generateSchdule(combination, labs);
+
+	const gened = await subjectController.findGened();
+
+	const ret = await subjectController.filterGened(generated, gened);
+	if (!ret.length) return res.status(400).send({ success: false });
+
+	return res.send(ret);
 });
 
 module.exports = router;
